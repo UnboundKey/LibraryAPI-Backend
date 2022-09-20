c374a0b2b0f2f68716095db554275b3644e80ca8
Benjamin Nordin <godiset@gmail.com>
2022-09-19 18:18:29 -0700

    Current setup Initial

---
+++ WebApi/Migrations/20220916002336_Initial.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    original_title = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    short_description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    coverImg = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    author_romanized = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ISBN = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    firstPrinted = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    printing = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    language = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    bookType = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
---
+++ WebApi/Migrations/20220918182456_add review field.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi.Migrations
+{
+    public partial class addreviewfield : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "review",
+                table: "Books",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "review",
+                table: "Books");
+        }
+    }
+}
