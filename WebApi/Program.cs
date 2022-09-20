aaa6d9e7547f1ba39204011b3b259aaf7126aa43
Benjamin Nordin <godiset@gmail.com>
2022-09-20 06:28:28 -0700

    Repository Pattern now works

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,9 @@
-using WebApi.Data;
+using DataAccess.EFCore;
+using DataAccess.EFCore.Repositories;
+using DataAccess.EFCore.UnitOfWork;
+using Domain.Interfaces;
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,9 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyHeader()));
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddDbContext<LibraryDBContext>();
-builder.Services.AddCors(c => c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyHeader()));
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+builder.Services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddTransient<IBookRepository, BookRepository>();
+
 var app = builder.Build();
 
 app.UseCors();
